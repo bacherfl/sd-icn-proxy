47268d99919a9e903f9db8107410e2ae0a41058c
Florian Bacher <bacherfl@gmail.com>
2015-04-28 15:13:26 +0200

    Initial commit

---
+++ src/test/java/itec/ProxyApplicationTests.java
@@ -0,0 +1,14 @@
+package itec;
+
+/*
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ProxyApplication.class)
+@WebAppConfiguration
+public class ProxyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+*/
