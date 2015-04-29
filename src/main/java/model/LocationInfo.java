357ff2d3924fbad9277bb909eafb18a00017fbb8
Florian Bacher <bacherfl@gmail.com>
2015-04-29 15:40:16 +0200

    current status

---
+++ src/main/java/model/LocationInfo.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by florian on 29.04.15.
+ */
+public class LocationInfo {
+
+    private List<String> locations;
+
+    public LocationInfo() {
+        locations = new ArrayList<>();
+    }
+
+    public List<String> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(List<String> locations) {
+        this.locations = locations;
+    }
+}
